a760eb039cb1b21571e96ed0fbc0219d8ed0af23
Hariom-Jangir <hariomjangir2006@gmail.com>
2026-03-19 19:34:44 +0530

    final resend setup

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,40 @@
 const express = require("express");
-const { Resend } = require("resend");
-const resend = new Resend(process.env.RESEND_API_KEY);
 const cors = require("cors");
 require("dotenv").config();
 
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
+// Test route
 app.get("/", (req, res) => {
-  res.send("Backend is running ");
+  res.send("Backend is running");
 });
 
+// Contact route
 app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
   console.log("Request received");
 
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-      },
-    });
-
     console.log("Before sending mail");
 
-   await resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: process.env.EMAIL,
-  subject: `New message from ${name}`,
-  text: `
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: process.env.EMAIL,
+      subject: `New message from ${name}`,
+      text: `
 Name: ${name}
 Email: ${email}
 Message: ${message}
-  `,
-});
+      `,
+    });
 
     console.log("After sending mail");
 
@@ -51,6 +46,7 @@ Message: ${message}
   }
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
